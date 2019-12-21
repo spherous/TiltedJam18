c59d00e7bc76125602248ec5c23d23aee0bf6a8b
Avea <aveakrause@gmail.com>
2019-12-21 14:16:47 -0800

    Projectile

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,5 +5,11 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance;
+    private void Awake() {
+        Instance = this;
+    }
+    
+    public Camera activeCamera;
     
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
@@ -6,23 +7,46 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
+    GameManager gm => GameManager.Instance;
     Rigidbody2D rb;
-    // [SerializeField]
-    // private float maxSpeed;
     [SerializeField]
     private float speed;
+
+    [SerializeField]
+    private GameObject projectile;
+
+    [SerializeField]
+    private float attackDelay;
+    private float lastAttackTime = 0;
 
     private void Awake() {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    private void Update() {
+    private void Update()
+    {
+        Movement();
+        if(Input.GetButton("Fire1"))
+            Fire();
+    }
+
+    private void Fire()
+    {
+        if(Time.time - lastAttackTime >= attackDelay)
+        {
+            lastAttackTime = Time.time;
+
+            GameObject newGO = Instantiate(projectile);
+            newGO.transform.position = transform.position;
+            newGO.GetComponent<Projectile>().Fire(gm.activeCamera.ScreenToWorldPoint(Input.mousePosition));
+        }
+    }
+
+    private void Movement()
+    {
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
 
-        // if(Mathf.Approximately(x, 0) && Mathf.Approximately(y, 0))
-        //     rb.velocity = new float2(0,0);
-
-        rb.AddForce(new float2(x,y) * speed * Time.deltaTime);
+        rb.AddForce(new float2(x, y) * speed * Time.deltaTime);
     }
 }
