dd421261ac47f13952c8cf9415019f5fb305716c
Avea <aveakrause@gmail.com>
2019-12-21 13:00:23 -0800

    Added bounds

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    public Transform target;
+    public Rect bounds;
+
+    private void Start() {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireCube(Vector3.zero, new Vector3(bounds.width * 0.5f, bounds.height * 0.5f, 0));
     }
 }
