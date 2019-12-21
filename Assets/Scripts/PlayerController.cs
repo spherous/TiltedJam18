82c5d7de6e1b958dfa5dac8cbd028627a21ec9a3
Avea <aveakrause@gmail.com>
2019-12-21 12:24:55 -0800

    Added Santa with movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    // [SerializeField]
+    // private float maxSpeed;
+    [SerializeField]
+    private float speed;
+
+    private void Awake() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update() {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        // if(Mathf.Approximately(x, 0) && Mathf.Approximately(y, 0))
+        //     rb.velocity = new float2(0,0);
+
+        rb.AddForce(new float2(x,y) * speed * Time.deltaTime);
+    }
+}
