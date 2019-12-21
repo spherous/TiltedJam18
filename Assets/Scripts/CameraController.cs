d261fcf94e2675e282d59a8c3b84cfb3e7245847
Avea <aveakrause@gmail.com>
2019-12-21 13:30:37 -0800

    camera bound work

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,18 +1,22 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using Unity.Mathematics;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
     public Transform target;
-    public Rect bounds;
-
-    private void Start() {
-        
+    public float2 bounds;
+    public float speed;
+    private void FixedUpdate() {
+        Vector3 pos = Vector3.Lerp(transform.position, target.position, speed * Time.fixedDeltaTime);
+        pos.x = Mathf.Clamp(pos.x, -bounds.x * 0.5f, bounds.x * 0.5f);
+        pos.y = Mathf.Clamp(pos.y, -bounds.y * 0.5f, bounds.y * 0.5f);
+        pos.z = -10;
+        transform.position = pos;
     }
-
+    
     private void OnDrawGizmos() {
+
         Gizmos.color = Color.blue;
-        Gizmos.DrawWireCube(Vector3.zero, new Vector3(bounds.width * 0.5f, bounds.height * 0.5f, 0));
+        Gizmos.DrawWireCube(Vector3.zero, new Vector3(bounds.x * 0.5f, bounds.y * 0.5f, 0));
     }
 }
