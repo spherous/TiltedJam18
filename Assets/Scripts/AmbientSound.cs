036bf4a509cadad5885774011fd20eef9702b2c8
DemonQ <biz@warpedimagination.com>
2019-12-21 21:37:52 -0800

    ambient background

---
+++ Assets/Scripts/AmbientSound.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AmbientSound : MonoBehaviour
+{
+    #region Constants
+
+
+    #endregion
+
+    #region Variables
+
+
+
+    #endregion
+
+    #region Properties
+
+
+    #endregion
+
+    #region Construction
+
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    #endregion
+
+    #region Update
+
+    void Update()
+    {
+        
+    }
+
+    #endregion
+
+    #region Management
+
+
+    #endregion
+
+}
