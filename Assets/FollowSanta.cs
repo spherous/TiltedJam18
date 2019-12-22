192a8e94804071a2522117e6c2351cad501e9e89
Max <maxaclark@gmail.com>
2019-12-21 21:27:49 -0800

    Auto stash before merge of "master" and "origin/master"
    Added layered night and santa vision. Failed to use 2d lights.

---
+++ Assets/FollowSanta.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowSanta : MonoBehaviour
+{
+    private Santa santa;
+
+    private bool santaDied = false;
+
+    private void Awake()
+    {
+        santa = FindObjectOfType<Santa>();
+        if (!santa) Destroy(gameObject);
+    }
+
+    private void Update()
+    {
+        transform.position = santa.transform.position;
+
+        if (santa.currentHealth <= 0)
+        {
+            santaDied = true;
+            StartCoroutine(ShrinkAndDie());
+        }
+    }
+
+    IEnumerator ShrinkAndDie ()
+    {
+        yield return null;
+    }
+}
