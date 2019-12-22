257c9b126b7a839a375a8f93847c6b2c23992ffb
DemonQ <biz@warpedimagination.com>
2019-12-21 16:52:13 -0800

    Intro screen added

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,14 +2,31 @@
 using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+
+    [SerializeField]
+    string m_startScreen = null;
+
+
+    public Camera activeCamera;
+
     public static GameManager Instance;
+
     private void Awake() {
         Instance = this;
     }
-    
-    public Camera activeCamera;
-    
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(m_startScreen);
+        }
+    }
+
+
 }
---
+++ Assets/Scripts/StartingScreen.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartingScreen : MonoBehaviour
+{
+    #region Constants
+
+
+    #endregion
+
+    #region Variables
+
+    [SerializeField]
+    string m_sceneToLoad = null;
+
+    #endregion
+
+    #region Properties
+
+
+    #endregion
+
+    #region Construction
+
+    void Start()
+    {
+        
+    }
+
+    #endregion
+
+    #region Update
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+        else
+        if (Input.anyKey)
+        {
+            SceneManager.LoadScene(m_sceneToLoad);
+        }
+    }
+
+    #endregion
+
+    #region Management
+
+
+    #endregion
+
+}
